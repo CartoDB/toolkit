1f48c7cad533047348cd3b86e8f5f50a9e36484c
Víctor Velarde <victor.velarde@gmail.com>
2019-09-26 14:01:58 +0200

    Revert "Refactor to Maps main class"
    This reverts commit c48c10406b1e696b7e2c6c853831ac2bbae0ed62.

--- packages/maps/__tests__/Maps.spec.ts
+++ packages/maps/__tests__/Maps.spec.ts
@@ -2,18 +2,8 @@ import Maps from '../src';
 
 describe('maps', () => {
   it('should have credentials', () => {
-    const m = new Maps('a-user', 'wadus');
-    expect(m.username).toBe('a-user');
+    const m = new Maps('roman-carto', 'wadus');
+    expect(m.username).toBe('roman-carto');
     expect(m.apiKey).toBe('wadus');
   });
-
-  it('can manage different servers', () =>{
-    const m = new Maps('a-user', 'wadus');
-    expect(m.serverURL).toBe('https://{user}.carto.com');
-
-    const customServer = 'http://127.0.0.1:8181/user/{user}';
-    const m2 = new Maps('a-user', 'wadus', customServer);
-    expect(m2.serverURL).toBe(customServer);
-  });
-
 });
--- packages/maps/src/index.ts
+++ packages/maps/src/index.ts
@@ -1,42 +1,29 @@
-const DEFAULT_SERVER_URL_TEMPLATE = 'https://{user}.carto.com';
-
 class Maps {
   private _username: string;
   private _apiKey: string;
-  private _serverURL: string;
 
-  /**
-   * Build an instance to interact with Maps API v1 (aka Windshaft)
-   * @param username
-   * @param apiKey
-   * @param serverURL A url pattern like default ('https://{user}.carto.com')
-   */
-  constructor(username: string, apiKey: string, serverURL: string = DEFAULT_SERVER_URL_TEMPLATE) {
+  constructor(username: string, apiKey: string) {
     this._username = username;
     this._apiKey = apiKey;
-    this._serverURL = serverURL;
   }
 
   public get username(): string {
     return this._username;
   }
 
+  public set username(value: string) {
+    this._username = value;
+  }
+
   public get apiKey(): string {
     return this._apiKey;
   }
 
-  public get serverURL(): string {
-    return this._serverURL;
+  public set apiKey(value: string) {
+    this._apiKey = value;
   }
 
-  public instantiateMap(options: any) {
-    const mapParameters = this.getMapParameters(options);
-    const requestOptions = this.getRequestOptions(mapParameters);
-
-    return this.fetchLayerGroup(requestOptions).then(this.getVectorTileURL);
-  }
-
-  private getMapParameters(options: { sql?: string, dataset?: string }) {
+  public instantiateMap(options: { sql?: string, dataset?: string }) {
     const { sql, dataset } = options;
 
     if (sql === undefined && dataset === undefined) {
@@ -53,31 +40,16 @@ class Maps {
       version: '1.3.1'
     };
 
-    return mapParameters;
-  }
-
-  private getRequestOptions(mapParameters: any) {
-    return {
+    const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(mapParameters)
     };
-  }
 
-  private fetchLayerGroup(requestOptions: any){
-    return fetch(`${this.apiBaseUrl()}?api_key=${this.apiKey}`, requestOptions)
-      .then((response) => response.json());
+    return fetch(`https://${this.username}.carto.com/api/v1/map?api_key=${this.apiKey}`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => `https://${this.username}.carto.com/api/v1/map/${data.layergroupid}/{z}/{x}/{y}.mvt?api_key=${this.apiKey}`);
   }
-
-  private getVectorTileURL(layerGroup: any){
-    return `${this.apiBaseUrl()}/${layerGroup.layergroupid}/{z}/{x}/{y}.mvt?api_key=${this.apiKey}`;
-  }
-
-  private apiBaseUrl() {
-    const baseForUser = this._serverURL.replace('{user}', this.username);
-    return `${baseForUser}/api/v1/map`;
-  }
-
 }
 
 export default Maps;
