ae1bd07fdda28092b89c1e28131bc7b2b26e2d8e
Víctor Velarde <victor.velarde@gmail.com>
2019-09-26 23:26:46 +0200

    feature(maps): refactor Maps

--- packages/maps/src/index.ts
+++ packages/maps/src/index.ts
@@ -1,54 +1,147 @@
-class Maps {
-  private _username: string;
-  private _apiKey: string;
 
-  constructor(username: string, apiKey: string) {
-    this._username = username;
-    this._apiKey = apiKey;
+const DEFAULT_SERVER_URL_TEMPLATE = 'https://{user}.carto.com';
+const DEFAULT_CLIENT_ID = 'maps-pkg';
+const REQUEST_GET_MAX_URL_LENGTH = 2048;
+
+class Maps {
+  private _conf: any;
+  private _auth: string;
+  private _client: string;
+
+  /**
+   * Build an instance to interact with Maps API v1 (aka Windshaft)
+   * @param username
+   * @param apiKey
+   * @param serverURL A url pattern like default ('https://{user}.carto.com')
+   *
+   */
+  constructor(username: string, apiKey: string, serverURL?: string, client?: string) {
+
+    this._conf = {
+      username,
+      apiKey,
+      serverUrlTemplate: serverURL || DEFAULT_SERVER_URL_TEMPLATE
+    };
+
+    this._auth = this.encodeParameter('api_key', this._conf.apiKey);
+    this._client = this.encodeParameter('client', client || DEFAULT_CLIENT_ID);
   }
 
   public get username(): string {
-    return this._username;
-  }
-
-  public set username(value: string) {
-    this._username = value;
+    return this._conf.username;
   }
 
   public get apiKey(): string {
-    return this._apiKey;
+    return this._conf.apiKey;
   }
 
-  public set apiKey(value: string) {
-    this._apiKey = value;
+  public get serverURL(): string {
+    return this._conf.serverUrlTemplate.replace('{user}', this._conf.username);
   }
 
-  public instantiateMap(options: { sql?: string, dataset?: string }) {
+  /**
+   * Instantiate a map based on dataset name or sql query, returning a layergroup
+   *
+   * Note: Get layergroup.metadata.tilejson.vector.tiles to consume MVT tiles
+   * @param options
+   */
+  public async instantiateSimpleMap(options: { sql?: string, dataset?: string }) {
     const { sql, dataset } = options;
 
     if (sql === undefined && dataset === undefined) {
       throw new Error('Please provide a dataset or a SQL query');
     }
 
-    const mapParameters = {
+    const mapConfig = {
       layers: [{
         type: 'cartodb',
         options: {
-            sql: sql || `select * from ${dataset}`
+          sql: sql || `select * from ${dataset}`
         }
       }],
       version: '1.3.1'
     };
 
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(mapParameters)
-    };
+    const layergroup = await this.instantiateMap(mapConfig);
+    return layergroup;
+  }
 
-    return fetch(`https://${this.username}.carto.com/api/v1/map?api_key=${this.apiKey}`, requestOptions)
-      .then((response) => response.json())
-      .then((data) => `https://${this.username}.carto.com/api/v1/map/${data.layergroupid}/{z}/{x}/{y}.mvt?api_key=${this.apiKey}`);
+  private async instantiateMap(mapConfig: any) {
+    let response;
+    try {
+      const payload = JSON.stringify(mapConfig);
+      response = await fetch(this.makeHttpRequest(payload));
+    } catch (error) {
+      throw new Error(`Failed to connect to Maps API with the user ('${this._conf.username}'): ${error}`);
+    }
+
+    const layergroup = await response.json();
+    if (!response.ok) {
+      this.dealWithWindshaftErrors(response, layergroup);
+    }
+
+    return layergroup;
+  }
+
+  private makeHttpRequest(payload: any) {
+    const parameters = [this._conf.auth, this._client, this.encodeParameter('config', payload)];
+    const url = this.generateUrl(this.generateMapsApiUrl(), parameters);
+    if (url.length < REQUEST_GET_MAX_URL_LENGTH) {
+      return this.getRequest(url);
+    }
+
+    return this.postRequest(payload);
+  }
+
+  private dealWithWindshaftErrors(response: any, layergroup: any) {
+    if (response.status === 401) {
+      throw new Error(
+        `Unauthorized access to Maps API: invalid combination of user ('${this._conf._username}') and apiKey ('${this._conf.apiKey}')`
+      );
+    } else if (response.status === 403) {
+      throw new Error(
+        `Unauthorized access to dataset: the provided apiKey('${this._conf.apiKey}') doesn't provide access to the requested data`
+      );
+    }
+    throw new Error(`${JSON.stringify(layergroup.errors)}`);
+  }
+
+  private getRequest(url: string) {
+    return new Request(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
+      }
+    });
+  }
+
+  private postRequest(payload: any) {
+    const parameters = [this._auth, this._client];
+
+    return new Request(this.generateUrl(this.generateMapsApiUrl(), parameters), {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: payload
+    });
+  }
+
+  private encodeParameter(name: string, value: string) {
+    return `${name}=${encodeURIComponent(value)}`;
+  }
+
+  private generateUrl(url: string, parameters: string[] = []) {
+    return `${url}?${parameters.join('&')}`;
+  }
+
+  private generateMapsApiUrl(path?: string) {
+    let url = `${this.serverURL}/api/v1/map`;
+    if (path) {
+      url += path;
+    }
+    return url;
   }
 }
 
