7da385f51cbecdc5bfde84a0c296113b85c191ac
Román Jiménez <drrouman@gmail.com>
2019-10-02 20:12:42 +0200

    Export all other packages on toolkit

--- packages/toolkit/src/index.ts
+++ packages/toolkit/src/index.ts
@@ -1,8 +1,13 @@
+import { OAuth } from '@carto/toolkit-auth';
+import { CustomStorage } from '@carto/toolkit-custom-storage';
+import { SQL } from '@carto/toolkit-sql';
 import App from './App';
 import OAuthApp from './OAuthApp';
 
 export default {
   App,
+  CustomStorage,
   OAuthApp,
-  // TODO: export all packages here, so this truly becomes a metapackage
+  OAuth,
+  SQL
 };
