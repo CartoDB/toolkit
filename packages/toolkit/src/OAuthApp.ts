7f30ffb3dcd086c66904a5572d6ca514b1f1fd16
Román Jiménez <drrouman@gmail.com>
2019-10-03 11:11:24 +0200

    setting client ID after creating the OAuthapp

--- packages/toolkit/src/OAuthApp.ts
+++ packages/toolkit/src/OAuthApp.ts
@@ -3,43 +3,64 @@ import { AuthParameters } from '@carto/toolkit-auth/dist/types/AuthParameters';
 import App, { AppOptions, DEFAULT_OPTIONS } from './App';
 
 export default class OAuthApp extends App {
-  private _oauth: OAuth;
+  private _oauth: OAuth | null = null;
+  private _oauthOptions: AuthParameters;
 
   constructor(oauthOptions: AuthParameters, options: AppOptions = DEFAULT_OPTIONS) {
     super(options);
+    this._oauthOptions = oauthOptions;
 
-    this._oauth = new OAuth(oauthOptions);
+    if (oauthOptions.clientID) {
+      this._oauth = new OAuth(oauthOptions);
+    }
   }
 
   /**
    * This function will start the OAuth flow, and will inherently call setCredentials.
    */
   public async login() {
-    let token = this._oauth.token;
+    const oauth = this.oauth;
 
-    if (this._oauth.expired) {
+    let token = oauth.token;
+
+    if (oauth.expired) {
       try {
-        await this._oauth.login();
+        await oauth.login();
       } catch (e) {
         throw new Error(`Failed to login ${e}`);
       }
 
-      token = this._oauth.token;
+      token = oauth.token;
     }
 
     if (token === null) {
       throw new Error(`Failed to login, token is null`);
     }
 
-    await this.postLogin(token);
+    await this.postLogin(oauth, token);
   }
 
   public get oauth(): OAuth {
+    if (this._oauth === null) {
+      throw new Error('No client ID has been specified');
+    }
+
     return this._oauth;
   }
 
-  private async postLogin(token: string) {
-    const userInfo = this._oauth.userInfo;
+  public setClientID(clientID: string) {
+    if (this._oauth) {
+      throw new Error('Cannot set the client ID more than once');
+    }
+
+    this._oauth = new OAuth({
+      clientID,
+      ...this._oauthOptions
+    });
+  }
+
+  private async postLogin(oauth: OAuth, token: string) {
+    const userInfo = oauth.userInfo;
 
     if (userInfo === null) {
       throw new Error(`Failed to get user info`);
@@ -49,16 +70,16 @@ export default class OAuthApp extends App {
 
     await this.setCredentials(token, info.username);
 
-    this.setupEvents();
+    this.setupEvents(oauth);
   }
 
-  private setupEvents() {
-    this._oauth.on('tokenUpdated', () => {
-      if (this._oauth.token === null) {
+  private setupEvents(oauth: OAuth) {
+    oauth.on('tokenUpdated', () => {
+      if (oauth.token === null) {
         throw new Error('Got a null token after refreshing it');
       }
 
-      this.setApiKey(this._oauth.token);
+      this.setApiKey(oauth.token);
     });
   }
 }
