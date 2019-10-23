f47fe6df4c60d53935a346b63acfdc1576b5794b
Raúl Yeguas <neokore@gmail.com>
2019-10-23 19:06:00 +0200

    Minify packages

--- packages/toolkit/rollup.config.ts
+++ packages/toolkit/rollup.config.ts
@@ -1,7 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import sourceMaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
 
 // tslint:disable-next-line:no-var-requires
 const { modules, umd } = require('../../rollup-config-generator');
@@ -13,10 +13,10 @@ export default [
     typescript({ useTsconfigDeclarationDir: true }),
     resolve(),
     commonjs(),
-    sourceMaps()
+    terser()
   ]),
   modules('src/index.ts', pkg, [
     typescript({ useTsconfigDeclarationDir: true }),
-    sourceMaps()
+    terser()
   ], Object.keys(pkg.dependencies))
 ];
