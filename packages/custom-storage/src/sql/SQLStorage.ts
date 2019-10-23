6d217c8978aad05228a8c9af310506cf5525673a
Román Jiménez <drrouman@gmail.com>
2019-10-23 17:21:25 +0200

    fix(custom-storage): upload dataset fixes (#33)
    * Fix independent dataset upload
    
    * Fix getDataset returning undefined instead of null
    
    * v0.0.1-rc.8

--- packages/custom-storage/src/sql/SQLStorage.ts
+++ packages/custom-storage/src/sql/SQLStorage.ts
@@ -105,7 +105,7 @@ export class SQLStorage {
       throw new Error(`Failed to get dataset ${name}`);
     }
 
-    return result.rows[0];
+    return result.rows[0] || null;
   }
 
   public async getDatasets(): Promise<StoredDataset[]> {
