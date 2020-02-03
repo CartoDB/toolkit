2ce862c400258d3ee112510f156be9ad53e966cd
Víctor Velarde <victor.velarde@gmail.com>
2020-02-03 22:43:40 +0100

    Remove unneeded mock iin test

--- packages/sql/__tests__/RequestManager.spec.ts
+++ packages/sql/__tests__/RequestManager.spec.ts
@@ -1,19 +1,6 @@
 import RequestManager from '../src/RequestManager';
 
 describe('RequestManager', () => {
-  beforeEach(() => {
-    const mockJson = Promise.resolve({
-      wadus: true,
-      username: 'roman-carto'
-    });
-
-    const mockFetch = Promise.resolve({
-      json: () => mockJson
-    });
-
-    (global as any).fetch = jest.fn().mockImplementation(() => mockFetch);
-  });
-
   it('should require username, api key and a server', () => {
     const rm = new RequestManager({ username: 'roman', apiKey: 'wadus', server: '{user}.wadus.com' });
 
