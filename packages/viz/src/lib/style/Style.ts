b314e96d743b776def38aa9f5d4eb41e16808d47
jesusbotella <jesus.botella@gmail.com>
2020-03-10 12:08:34 +0100

    Add hasProperties and named export to Style

--- packages/viz/src/lib/style/Style.ts
+++ packages/viz/src/lib/style/Style.ts
@@ -1,15 +1,17 @@
 import { GeoJsonLayerProps } from '@deck.gl/layers/geojson-layer/geojson-layer';
 
-class Style {
+export class Style {
   private _styleProperties: GeoJsonLayerProps<any>;
 
   constructor(styleProperties: GeoJsonLayerProps<any>) {
     this._styleProperties = {...styleProperties};
   }
 
+  public hasProperties() {
+    return Boolean(Object.keys(this._styleProperties).length);
+  }
+
   public getProperties() {
     return this._styleProperties;
   }
 }
-
-export default Style;
