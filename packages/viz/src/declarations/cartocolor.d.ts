abf5040562d3223d2bfac2975fe89a0ae1ebbdc3
jesusbotella <jesus.botella@gmail.com>
2020-03-17 13:40:25 +0100

    Fix tests

--- packages/viz/src/declarations/cartocolor.d.ts
+++ packages/viz/src/declarations/cartocolor.d.ts
@@ -2,7 +2,4 @@ declare module 'cartocolor' {
   export interface SubPalette {
     [paletteIndex: string]: string[]
   }
-
-  const cartocolorPalettes: Record<string, Record<string, string[]>>;
-  export default cartocolorPalettes;
 }
--- packages/viz/src/lib/style/palettes/index.ts
+++ packages/viz/src/lib/style/palettes/index.ts
@@ -1,14 +1,17 @@
-import cartoColors from 'cartocolor';
+import * as cartoColors from 'cartocolor';
 import { CartoStylingError, stylingErrorTypes } from '../../errors/styling-error';
 import { Palette } from './palette';
 
 export const palettes: Record<string, Palette> = {};
 
 // CARTOColors
+// Workaround for type when importing this commonjs module
+const cartoPalette: Record<string, Record<string, string[]>> = cartoColors;
+
 Object
   .keys(cartoColors)
   .map((name) => {
-    palettes[`${name.toUpperCase()}`] = new Palette(name, cartoColors[name]);
+    palettes[`${name.toUpperCase()}`] = new Palette(name, cartoPalette[name]);
   });
 
 export function getColorPalette(paletteName: string, numberOfCategories: number) {
