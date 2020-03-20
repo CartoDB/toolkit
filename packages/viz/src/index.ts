56c8d7e1ffa4ca0c253fc1b23e54578e10bec8be
jesusbotella <jesus.botella@gmail.com>
2020-03-20 11:28:46 +0100

    Remove setDefaultCredentials export from viz package

--- packages/viz/src/index.ts
+++ packages/viz/src/index.ts
@@ -1,7 +1,3 @@
-// TODO: Should we export setDefaultCredentials from here,
-// or just from top package?
-export { setDefaultCredentials } from '@carto/toolkit-core';
-
 export { Layer } from './lib/Layer';
 
 // Style helpers
