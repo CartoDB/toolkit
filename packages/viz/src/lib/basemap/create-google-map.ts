a2f84bb0f29873c8563070be3db1e905c673a6ad
Víctor Velarde <victor.velarde@gmail.com>
2020-04-28 14:53:45 +0200

    refactor: gmaps ctor params

--- packages/viz/src/lib/basemap/create-google-map.ts
+++ packages/viz/src/lib/basemap/create-google-map.ts
@@ -1,9 +1,17 @@
 import { CartoBaseMapError } from '../errors/basemap-error';
 
-const DEFAULT_OPTIONS: google.maps.MapOptions = {
-  mapTypeId: 'roadmap',
-  center: { lat: 0, lng: 0 },
-  zoom: 1
+interface GoogleMapOptions {
+  container?: Element | string;
+  mapOptions?: google.maps.MapOptions;
+}
+
+const DEFAULT_OPTIONS: GoogleMapOptions = {
+  container: 'map',
+  mapOptions: {
+    mapTypeId: 'roadmap',
+    center: { lat: 0, lng: 0 },
+    zoom: 1
+  }
 };
 
 /**
@@ -13,35 +21,31 @@ const DEFAULT_OPTIONS: google.maps.MapOptions = {
  * ```javascript
  *    // Several options to create the map are allowed
  *    const deckMap = carto.viz.createGoogleMap();
- *    const deckMap = carto.viz.createGoogleMap('map', { mapTypeId: 'satellite'});
- *    const deckMap = carto.viz.createGoogleMap('map', { mapTypeId: 'hybrid', zoom: 4 });
- *    const deckMap = carto.viz.createGoogleMap('map', { mapTypeId: 'terrain', zoom: 4, center: { lng: 3, lat: 40 } });
+ *    const deckMap = carto.viz.createGoogleMap({ mapOptions: { mapTypeId: 'satellite'} });
+ *    const deckMap = carto.viz.createGoogleMap({ mapOptions: { mapTypeId: 'hybrid', zoom: 4 } });
+ *    const deckMap = carto.viz.createGoogleMap({ container: 'map', mapOptions: { mapTypeId: 'terrain', zoom: 4, center: { lng: 3, lat: 40 } } });
  * ```
  * @export
- * @param {Element | string} container
- * @param {google.maps.MapOptions} googleMapsOptions
+ * @param {GoogleMapOptions} options
  * @returns
  */
-export function createGoogleMap(
-  container = 'map',
-  options: google.maps.MapOptions = DEFAULT_OPTIONS
-) {
+export function createGoogleMap(options: GoogleMapOptions = DEFAULT_OPTIONS) {
   if (!window.google.maps.Map) {
     throw new CartoBaseMapError(
       'Google Maps JavaScript API not found within window context. Please check documentation to know more about how to configure it.'
     );
   }
 
-  const mapOptions = { ...DEFAULT_OPTIONS, ...options };
+  const container = options.container || DEFAULT_OPTIONS.container || 'map';
   const element =
     typeof container === 'string'
       ? (document.getElementById(container) as Element)
       : container;
 
-  // Google Maps setup
-  const baseMap = new window.google.maps.Map(element, mapOptions);
+  const mapOptions = { ...DEFAULT_OPTIONS.mapOptions, ...options.mapOptions };
 
-  // Deckgl as an overlay
+  // Google Maps setup & DeckGL as an overlay
+  const baseMap = new window.google.maps.Map(element, mapOptions);
   const deckOverlay = new (window.deck.GoogleMapsOverlay as any)({
     layers: []
   });
