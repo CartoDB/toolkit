731f88a7f850c41ab52926997f64e4bfa85b52bc
jesusbotella <jesus.botella@gmail.com>
2020-05-22 11:02:27 +0200

    Export WithEvents class

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,10 @@
-export { Credentials, defaultCredentials, setDefaultCredentials } from './Credentials';
 export { default as MetricsEvent } from './MetricsEvent';
 export { CartoError } from './error/carto-error';
+
+export {
+  Credentials,
+  defaultCredentials,
+  setDefaultCredentials
+} from './Credentials';
+
+export { WithEvents } from './mixins';
