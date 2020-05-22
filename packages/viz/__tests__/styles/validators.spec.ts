c6c194092a8f50a3eb7d3e85c5d2d0fd498db0a7
Simon Martín <oleurud@gmail.com>
2020-05-22 17:21:25 +0200

    validateBasicParameters

--- packages/viz/__tests__/styles/validators.spec.ts
+++ packages/viz/__tests__/styles/validators.spec.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-restricted-syntax */
 
-import { colorValidation } from '../../src/lib/style/helpers/validators';
+import { colorValidation } from '../../src/lib/style/validators';
+import { getStyles } from '../../src/lib/style/deck-styles';
+import { CartoStylingError } from '../../src/lib/errors/styling-error';
 
 describe('Validators', () => {
   describe('colorValidation', () => {
@@ -27,6 +29,117 @@ describe('Validators', () => {
       for (const color of colors) {
         expect(colorValidation(color)).toBe(true);
       }
+    });
+  });
+
+  describe('validateBasicParameters', () => {
+    const geometryType = 'Point';
+    const validColor = '#000000';
+    const invalidColor = '#';
+
+    it('fails with invalid color', () => {
+      const options = {
+        color: invalidColor,
+        size: 1,
+        opacity: 1,
+        strokeColor: validColor,
+        strokeWidth: 1
+      };
+
+      try {
+        getStyles(geometryType, options);
+      } catch (error) {
+        expect(error).toBeInstanceOf(CartoStylingError);
+      }
+    });
+
+    it('fails with size smaller than 1', () => {
+      const options = {
+        color: validColor,
+        size: 0,
+        opacity: 1,
+        strokeColor: validColor,
+        strokeWidth: 1
+      };
+
+      try {
+        getStyles(geometryType, options);
+      } catch (error) {
+        expect(error).toBeInstanceOf(CartoStylingError);
+      }
+    });
+
+    it('fails with opacity smaller than 0', () => {
+      const options = {
+        color: validColor,
+        size: 1,
+        opacity: -1,
+        strokeColor: validColor,
+        strokeWidth: 1
+      };
+
+      try {
+        getStyles(geometryType, options);
+      } catch (error) {
+        expect(error).toBeInstanceOf(CartoStylingError);
+      }
+    });
+
+    it('fails with opacity greather than 1', () => {
+      const options = {
+        color: validColor,
+        size: 1,
+        opacity: 1.1,
+        strokeColor: validColor,
+        strokeWidth: 1
+      };
+
+      try {
+        getStyles(geometryType, options);
+      } catch (error) {
+        expect(error).toBeInstanceOf(CartoStylingError);
+      }
+    });
+
+    it('fails with invalid strokeColor', () => {
+      const options = {
+        color: validColor,
+        size: 1,
+        opacity: 1,
+        strokeColor: invalidColor,
+        strokeWidth: 1
+      };
+
+      expect(() => getStyles(geometryType, options)).toThrow(CartoStylingError);
+    });
+
+    it('fails with strokeWidth smaller than 0', () => {
+      const options = {
+        color: validColor,
+        size: 1,
+        opacity: 1,
+        strokeColor: validColor,
+        strokeWidth: -1
+      };
+
+      try {
+        getStyles(geometryType, options);
+      } catch (error) {
+        expect(error).toBeInstanceOf(CartoStylingError);
+      }
+    });
+
+    it('works with values in the limit', () => {
+      const options = {
+        color: '#000',
+        size: 1,
+        opacity: 0,
+        strokeColor: '#FFFFFFFF',
+        strokeWidth: 0
+      };
+
+      const style = getStyles(geometryType, options);
+      expect(style).toBeTruthy();
     });
   });
 });
--- packages/viz/src/lib/style/validators.ts
+++ packages/viz/src/lib/style/validators.ts
@@ -6,6 +6,6 @@ function _isHexcolor(color: string) {
   return (
     color.startsWith('#') &&
     [4, 7, 9].includes(color.length) &&
-    !!Number(`0x${color.substring(1)}`)
+    !isNaN(Number(`0x${color.substring(1)}`)) // eslint-disable-line no-restricted-globals
   );
 }
