c2ca5b3c252c18765fd7796f0bc653e54ec6821b
Simon Martín <oleurud@gmail.com>
2020-06-09 17:33:48 +0200

    GeoJSON types

--- packages/viz/src/lib/sources/GeoJsonTypes.ts
+++ packages/viz/src/lib/sources/GeoJsonTypes.ts
@@ -1,44 +1,82 @@
 /**
- * GeoJson types
- * https://tools.ietf.org/html/rfc7946#section-1.4
+ * Based on https://tools.ietf.org/html/rfc7946
  */
-export type GeoJsonTypes = [
-  'Feature',
-  'FeatureCollection',
-  'Point',
-  'MultiPoint',
-  'LineString',
-  'MultiLineString',
-  'Polygon',
-  'MultiPolygon',
-  'GeometryCollection'
-];
 
-/**
- * GeoJson types
- * https://tools.ietf.org/html/rfc7946#section-1.4
- */
-export type GeometryTypes = [
-  'Point',
-  'MultiPoint',
-  'LineString',
-  'MultiLineString',
-  'Polygon',
-  'MultiPolygon',
-  'GeometryCollection'
-];
+import { GeometryType } from './Source';
 
-/**
- * Bounding box
- * https://tools.ietf.org/html/rfc7946#section-5
- */
-export type BBox = [number, number, number, number];
+type GeoJsonGeometryType =
+  'Point' |
+  'MultiPoint' |
+  'LineString' |
+  'MultiLineString' |
+  'Polygon' |
+  'MultiPolygon';
 
-/**
- * GeoJson
- * https://tools.ietf.org/html/rfc7946#section-3
- */
-export interface GeoJsonObject {
-  type: GeoJsonTypes;
+type BBox = [number, number, number, number];
+type Coordinates = number[];
+
+interface GeoJsonObject {
+  type: GeoJsonGeometryType | 'Feature' | 'FeatureCollection' | 'GeometryCollection';
   bbox?: BBox;
+}
+
+interface Feature extends GeoJsonObject {
+  type: 'Feature';
+  geometry: Geometry;
+  properties?: { [name: string]: any; } | null;
+}
+
+interface FeatureCollection extends GeoJsonObject {
+  type: 'FeatureCollection';
+  features: Array<Geometry>;
+}
+
+interface Point extends GeoJsonObject {
+  type: 'Point';
+  coordinates: Coordinates;
+}
+
+interface MultiPoint extends GeoJsonObject {
+  type: 'MultiPoint';
+  coordinates: Coordinates[];
+}
+
+interface LineString extends GeoJsonObject {
+  type: 'LineString';
+  coordinates: Coordinates[];
+}
+
+interface MultiLineString extends GeoJsonObject {
+  type: 'MultiLineString';
+  coordinates: Coordinates[][];
+}
+
+interface Polygon extends GeoJsonObject {
+  type: 'Polygon';
+  coordinates: Coordinates[][];
+}
+
+interface MultiPolygon extends GeoJsonObject {
+  type: 'MultiPolygon';
+  coordinates: Coordinates[][][];
+}
+
+interface GeometryCollection extends GeoJsonObject {
+  type: 'GeometryCollection';
+  geometries: Geometry[];
+}
+
+type Geometry = Point | MultiPoint | LineString | MultiLineString | Polygon | MultiPolygon;
+export type GeoJSON = FeatureCollection | Feature | Geometry | GeometryCollection;
+
+export function parseGeometryType (geoJsonGeometryType: GeoJsonGeometryType): GeometryType {
+  if (['Point', 'MultiPoint'].includes(geoJsonGeometryType)) {
+    return 'Point'
+  }
+
+  if (['LineString', 'MultiLineString'].includes(geoJsonGeometryType)) {
+    return 'Line'
+  }
+
+  return 'Polygon'
 }
